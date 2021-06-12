e1d2f8ab4c4956f4693aeda635c20bc08a7d6c83
pranav-coder2005 <82258727+pranav-coder2005@users.noreply.github.com>
2021-06-12 15:43:11 +0530

    Update diabetes_detector_app.py

--- diabetes_detector_app.py
+++ diabetes_detector_app.py
@@ -186,5 +186,7 @@ st.write("World Diabetes Day: 14 November")
 
 st.sidebar.subheader("""An article about this app (https://proskillocity.blogspot.com/2021/04/official-launch-of-our-first-web-app.html)""")
 st.write("Dataset citation : Smith, J.W., Everhart, J.E., Dickson, W.C., Knowler, W.C., & Johannes, R.S. (1988).  Using the ADAP learning algorithm to forecast the onset of diabetes mellitus. In Proceedings of the Symposium on Computer Applications and Medical Care (pp. 261--265). IEEE Computer Society Press.")
+st.write("Original owners of the dataset: Original owners: National Institute of Diabetes and Digestive and Kidney Diseases (b) Donor of database: Vincent Sigillito (vgs@aplcen.apl.jhu.edu) Research Center, RMI Group Leader Applied Physics Laboratory The Johns Hopkins University Johns Hopkins Road Laurel, MD 20707 (301) 953-6231 © Date received: 9 May 1990")
+st.write("Dataset License: Open Data Commons Public Domain Dedication and License (PDDL)")
 
 st.write("Disclaimer: This is just a learning project based on one particular dataset so please do not depend on it to actually know if you have diabetes or not. It might still be a false positive or false negative. A doctor is still the best fit for the determination of such diseases.")
